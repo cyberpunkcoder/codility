a4da02ad21f7e0120f16fa911672725b13a26656
cyberpunkcoder <cyberpunkcoder@gmail.com>
2021-02-19 19:31:45 -0800

    added reference

---
+++ solution/main.go
@@ -0,0 +1,28 @@
+package main
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func main() {
+	// Solution()
+}
+
+/*
+	// Solution for problem
+	func Solution(N int) int {
+
+		return 0 // REMEMBER TO CHANGE RETURN
+	}
+*/
+
+/*
+	// Solution2 to problem
+	func Solution2(N int) int {
+
+		return 0 // REMEMBER TO CHANGE RETURN
+	}
+*/
---
+++ solution/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+	// Add "go.testFlags": ["-v"], to settings.json to view output
+
+	// IMPROVED TESTING LOOP
+	========================
+
+	// Record runtime
+	start := time.Now()
+
+	// Function call goes here
+	got :=
+
+	duration := time.Since(start)
+
+	// Print the arguments, the returns and runtime
+	fmt.Println("Gave:", tt.args)
+	fmt.Println("Got:", got)
+	fmt.Println("Time:", duration)
+*/
