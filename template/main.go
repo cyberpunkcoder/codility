619ee892cc3db9435c1dfc584894b0a1317a3221
cyberpunkcoder <cyberpunkcoder@gmail.com>
2021-02-20 17:39:29 -0800

    removed comments

--- template/main.go
+++ template/main.go
@@ -12,19 +12,3 @@ func main() {
 }
 
 // Solution to the problem
-
-/*
-	// Solution for problem
-	func Solution(N int) int {
-
-		return 0 // REMEMBER TO CHANGE RETURN
-	}
-*/
-
-/*
-	// Solution2 to problem
-	func Solution2(N int) int {
-
-		return 0 // REMEMBER TO CHANGE RETURN
-	}
-*/
